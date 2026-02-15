ba769bcfbef1c51d7487fae4e6fb71aaa614be87
Mike Morelli <mmorelli@meta.com>
2026-02-14 16:00:41 -0800

    Initial Commit

---
+++ Darty/DartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartyApp.swift
+//  Darty
+//
+//  Created by Mike Morelli on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct DartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
